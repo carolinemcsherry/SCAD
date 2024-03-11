b2bd7c3bc55c5acf2d032f3b0808f57bff2b639a
CMC18 <40626785@live.napier.ac.uk>
2024-03-11 13:00:29 +0000

    Missing Import

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-java.util.ArrayList;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
