b184f546833445fb620079574138c40ee75246c9
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-11 14:20:13 +0000

    Updated Java application to now pull the total number of cities and display as a printline

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,4 +1,5 @@
 package com.napier.sem;
+
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class App {
+
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
@@ -14,70 +16,147 @@ public class App {
         a.connect();
         System.out.println("Database has successfully connected and will now pull a query");
 
-
+        // Retrieve and print city information
+        ArrayList<City> cities = a.getAllCities();
+        System.out.println("There are a total of " + cities.size() + " cities in the database");
 
         // Disconnect from database
         a.disconnect();
         System.out.println("Database has successfully disconnected");
-
     }
 
     private Connection con = null;
 
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
 
-
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
+    public ArrayList<City> getAllCities() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
 
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT ID, Name, CountryCode, District, Population " +
+                            "FROM city " +
+                            "ORDER BY ID ASC";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Extract city information
+            ArrayList<City> cities = new ArrayList<>();
+            while (rset.next()) {
+                City city = new City();
+                city.setId(rset.getInt("ID"));
+                city.setName(rset.getString("Name"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setDistrict(rset.getString("District"));
+                city.setPopulation(rset.getInt("Population"));
+                cities.add(city);
+            }
+
+            return cities;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    public class City {
+        private int id;
+        private String name;
+        private String countryCode;
+        private String district;
+        private int population;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCountryCode() {
+            return countryCode;
+        }
+
+        public void setCountryCode(String countryCode) {
+            this.countryCode = countryCode;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        public void setDistrict(String district) {
+            this.district = district;
+        }
+
+        public int getPopulation() {
+            return population;
+        }
+
+        public void setPopulation(int population) {
+            this.population = population;
+        }
+
+        // You may want to override the toString() method to print the City object nicely
+        @Override
+        public String toString() {
+            return "City{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", countryCode='" + countryCode + '\'' +
+                    ", district='" + district + '\'' +
+                    ", population=" + population +
+                    '}';
+        }
+    }
 }
