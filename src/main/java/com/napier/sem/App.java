3850418b698e3f9f1bda7ec323950b94139f4459
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-11 11:39:53 +0000

    Updated docker-compose.yml, App,Java and created a config file for the db that is not being used. Hopefully fixed connection issues, not sure how

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,6 +9,7 @@ public class App {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Successfuly loaded jdbc.driver");
         } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
@@ -16,19 +17,25 @@ public class App {
 
         // Connection to the database
         Connection con = null;
+        System.out.println("Connection is null");
         int retries = 100;
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
+                System.out.println("waiting 30 seconds");
                 Thread.sleep(30000);
+                System.out.println("30 seconds is now over");
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?useSSL=false", "root", "example");
+                System.out.println("Attempting connection to jdbc:mysql://db:3306/world?useSSL=false");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
 
 
                 System.out.println("Successfully connected");
                 // Wait a bit
+
                 Thread.sleep(10000);
+
                 // Exit for loop
                 break;
             } catch (SQLException sqle) {
