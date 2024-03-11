528f787fbb82b0500ee02319cebd16225f90a8e3
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-11 13:11:10 +0000

    Added 2 methods to Java.app, dockerfile has been recreated for Database. Mytest.java has been removed

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,55 +7,77 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        try {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        System.out.println("Database has successfully connected and will now pull a query");
+
+
+
+        // Disconnect from database
+        a.disconnect();
+        System.out.println("Database has successfully disconnected");
+
+    }
+
+    private Connection con = null;
+
+    public void connect()
+    {
+        try
+        {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Successfuly loaded jdbc.driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        System.out.println("Connection is null");
-        int retries = 100;
-        for (int i = 0; i < retries; ++i) {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
+            try
+            {
                 // Wait a bit for db to start
-                System.out.println("waiting 30 seconds");
                 Thread.sleep(30000);
-                System.out.println("30 seconds is now over");
                 // Connect to database
-                System.out.println("Attempting connection to jdbc:mysql://db:3306/world?useSSL=false");
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-
-
                 System.out.println("Successfully connected");
-                // Wait a bit
-
-                Thread.sleep(10000);
-
-                // Exit for loop
                 break;
-            } catch (SQLException sqle) {
+            }
+            catch (SQLException sqle)
+            {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
+            }
+            catch (InterruptedException ie)
+            {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
-        if (con != null) {
-            try {
+
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
                 // Close connection
                 con.close();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 System.out.println("Error closing connection to database");
             }
-            System.out.println("We are now ready to pull from the database");
         }
     }
-}
 
+
+}
