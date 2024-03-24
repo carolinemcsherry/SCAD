ea421222443dabf1af82c6deecd075a02ac3386e
CMC18 <40626785@live.napier.ac.uk>
2024-03-24 13:11:54 +0000

    Saved same as dev

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,31 +6,48 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
+/*
+ * Main appliacation to run reports
+ * */
 
 public class App {
 
     public static void main(String[] args) {
-        // Get user input to pick report. Returns chosen report as string
-        //joption
-        //   String SelectedReport = User_Input.UserReport();
-
         // Connect to database
         Connection con = connect();
-        //log post
-        System.out.println("out of connect");
+        // user report 2
+        ArrayList<User_report_2.CapitalCityReport> ReportArray = User_report_2.getAllCapitalCities(con);
 
-        ArrayList<User_report_1.CountryPopulationReport> ReportArray1 = User_report_1.getAllCountriesPopulation(con);
-        //consoul
-        User_report_1.printAllCountriesPopulation(ReportArray1);
+        User_report_2.printCapitalCityReport(ReportArray);
+// user report 3
+        ArrayList<User_report_3.CityReport> ReportArray1 = User_report_3.getCityReport(con);
 
-        //Menu.runMenu(con);
-        //Go in to switch to build chosen report passes selected report string and connection
-     /*  Switch_Report.ReportArray(SelectedReport, con);
+        User_report_3.printCityReport(ReportArray1);
+// user report 4
+        ArrayList<User_report_4.CountryReport> ReportArray2 = User_report_4.getAllCountries(con);
 
-        //log post
-        System.out.println("out of ArrayList.");
-        System.out.println("Going in to if.");*/
+        User_report_4.printCountryReport(ReportArray2);
+// user report 5
+        ArrayList<User_report_5.LanguageStats> ReportArray3 = User_report_5.getLanguageStatistics(con);
+
+        User_report_5.printLanguageStatistics(ReportArray3);
+// user report 6
+        ArrayList<User_report_6.CityReport> ReportArray4 = User_report_6.getAllCities(con);
+
+        User_report_6.printCityReport(ReportArray4);
+// user report 7
+        ArrayList<User_report_7.RegionPopulationReport> ReportArray5 = User_report_7.getRegionPopulation(con);
+
+        User_report_7.printRegionPopulation(ReportArray5);
+// user report 8
+        ArrayList<User_report_8.CountryPopulation> ReportArray6 = User_report_8.getCountryPopulation(con);
+
+        User_report_8.printCountryPopulation(ReportArray6);
+// user report 9
+        ArrayList<User_report_9.RegionPopulationReport> ReportArray7 = User_report_9.getAllRegionsPopulation(con);
+
+        User_report_9.printAllRegionsPopulation(ReportArray7);
+
         // Disconnect from database
         disconnect();
         System.out.println("Database has successfully disconnected");
@@ -55,7 +72,7 @@ public class App {
             System.out.println("going in to try");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(5000);
                 // Connect to database
                 System.out.println("going in to con");
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
