53513132eb94746c7889162bf0f999c56e677daa
djackson <40620646@live.napier.ac.uk>
2024-03-24 18:01:46 +0000

    Test 12

--- src/main/java/com/napier/sem/User_report_12.java
+++ src/main/java/com/napier/sem/User_report_12.java
@@ -69,7 +69,7 @@ public class User_report_12 {
 
     // Method to print top N populated capital cities in a continent
     public static void printTopCapitalCitiesInContinentReport(ArrayList<User_report_12.TopCapitalCitiesInContinent> TopCapitalCitiesInContinent) {
-        // Check if ArrayList is not null
+        // Check if ArrayList is not null.
         if (TopCapitalCitiesInContinent == null) {
             System.out.println("No TopCapitalCitiesInContinent");
             return;
