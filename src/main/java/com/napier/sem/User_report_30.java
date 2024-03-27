1badb0d192df785bcc9ddec105afb5e11ce22875
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-27 14:12:12 +0000

    updated output for loop

--- src/main/java/com/napier/sem/User_report_30.java
+++ src/main/java/com/napier/sem/User_report_30.java
@@ -70,5 +70,14 @@ public class User_report_30{
         System.out.println(String.format("%-25s %-25s %-25s %-25s","countryName","population"));
 
 
+        for (CountryInRegion countries : countriesList) {
+            if (countriesList == null)
+                continue;
+            //Prints table values in columbs
+            String Table_string =
+                    String.format("%-25s %-25s %-25s %-25s",
+                            countries.countryName,countries.population);
+            System.out.println(Table_string);
+
     }
 }
