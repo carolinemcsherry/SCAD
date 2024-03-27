55bae6fd58eee845c7aa1b3751a53ac624327eb9
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-27 13:17:34 +0000

    updated reports

--- src/main/java/com/napier/sem/User_report_30.java
+++ src/main/java/com/napier/sem/User_report_30.java
@@ -22,8 +22,8 @@ public class User_report_30{
 
         // Method to represent the object as a string
         public String toString() {
-            return "Country Name: " + countryName + ", " +
-                    "Population: " + population;
+            return countryName
+                    + population;
         }
     }
 
