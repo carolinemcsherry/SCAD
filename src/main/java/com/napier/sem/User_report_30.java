60a712864b5084867ca65848bf6e697c5a53d3b9
Stuart Moore <40623146@live.napier.ac.uk>
2024-03-27 14:08:37 +0000

    remove For loop

--- src/main/java/com/napier/sem/User_report_30.java
+++ src/main/java/com/napier/sem/User_report_30.java
@@ -69,9 +69,6 @@ public class User_report_30{
         }
         System.out.println(String.format("%-25s %-25s %-25s %-25s","countryName","population"));
 
-        System.out.println("Countries by Region Report:");
-        for (CountryInRegion country : countriesList) {
-            System.out.println(country);
-        }
+
     }
 }
