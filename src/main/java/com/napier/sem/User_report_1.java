adbc2f94781c9d58c5caff93048f610fbb63b023
djackson <40620646@live.napier.ac.uk>
2024-03-28 11:12:31 +0000

    User 1 changes

--- src/main/java/com/napier/sem/User_report_1.java
+++ src/main/java/com/napier/sem/User_report_1.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-// 1The population of people, people living in cities, and people not living in cities in each region.
+// User Report 1 - The population of people, people living in cities, and people not living in cities in each region.
 
 public class User_report_1 {
 
