4db62bf8711911f0d49e8a8f932a736dda579017
djackson <40620646@live.napier.ac.uk>
2024-03-28 11:35:09 +0000

    User 1 changes

--- src/main/java/com/napier/sem/User_report_2.java
+++ src/main/java/com/napier/sem/User_report_2.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class User_report_2{
 
     //Capital City Report
-    
+
 /*A Capital city report requires the following columns:
 
 Name.
@@ -85,3 +85,5 @@ Population*/
         }
     }
 }
+
+//
