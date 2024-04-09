52623c8284b2595aff0b485c8078e639711a2bd9
Stuart-Moore-40623146 <40623146@live.napier.ac.uk>
2024-04-09 14:04:11 +0100

    updated user report

--- src/main/java/com/napier/sem/User_report_28.java
+++ src/main/java/com/napier/sem/User_report_28.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 // the population of people, people living in cities, and people not living in cities in each country
 public class User_report_28{
