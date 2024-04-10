9d5cb76bd794fa5ebbd74ab51ec5154408a22fac
CMC18 <40626785@live.napier.ac.uk>
2024-04-10 17:00:13 +0100

    gettin country population

--- src/main/java/com/napier/sem/User_report_9.java
+++ src/main/java/com/napier/sem/User_report_9.java
@@ -1,5 +1,6 @@
 package com.napier.sem;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,47 +23,40 @@ public class User_report_9{
 
         private String Region;
         private long Total_Region_Population;
-        private long Total_City_Population;
-        private String City_Population_Percentage;
-        private long Total_Not_City_Population;
-        private String None_City_Population_Percentage;
+
 
         // Constructor for the RegionPopulationReport class
-        public RegionPopulationReport(String Region, long Total_Region_Population, long Total_City_Population, String City_Population_Percentage, long Total_Not_City_Population, String None_City_Population_Percentage) {
+        public RegionPopulationReport(String Region, long Total_Region_Population) {
             this.Region = Region;
             this.Total_Region_Population = Total_Region_Population;
-            this.Total_City_Population = Total_City_Population;
-            this.City_Population_Percentage = City_Population_Percentage;
-            this.Total_Not_City_Population = Total_Not_City_Population;
-            this.None_City_Population_Percentage = None_City_Population_Percentage;
+
         }
 
         // Method to represent the object as a string
         public String toString() {
             return   Region +
-                    Total_Region_Population +
-                     Total_City_Population +
-                     Total_Not_City_Population +
-                    City_Population_Percentage +
-                    None_City_Population_Percentage;
+                    Total_Region_Population ;
         }
     }
 
     // Method to retrieve population data for all regions
     public static ArrayList<RegionPopulationReport> getAllRegionsPopulation(Connection con) {
+
+        String input = JOptionPane.showInputDialog("Enter the name of the Region or leave blank for all Region's");
+
+        if (input.isEmpty() == true) {
+            input = "%";
+            System.out.println(input);
+        }
+
         try {
             Statement stmt = con.createStatement();
 
             // SQL query to retrieve region-wise population data
             String strSelect = "SELECT country.Region, " +
-                    "SUM(country.Population) AS Total_Region_Population, " +
-                    "SUM(city.Population) AS Total_City_Population, " +
-                    "CONCAT(ROUND(SUM(city.Population) / SUM(country.Population) * 100, 2),'%') AS City_Population_Percentage, " +
-                    "SUM(country.Population) - SUM(city.Population) AS Total_Not_City_Population, " +
-                    "CONCAT(100 - ROUND(SUM(city.Population) / SUM(country.Population) * 100, 2) ,'%') AS None_City_Population_Percentage " +
-                    "FROM country " +
-                    "JOIN city ON country.Code = city.CountryCode " +
-                    "GROUP BY country.Region";
+                    "SUM(country.Population) AS Total_Region_Population " +
+                    "FROM country where country.Region like '" + input + "' GROUP BY country.Region";
+
 
             ResultSet rset = stmt.executeQuery(strSelect);
 
@@ -72,13 +66,10 @@ public class User_report_9{
             while (rset.next()) {
                 String Region = rset.getString("Region");
                 long Total_Region_Population = rset.getLong("Total_Region_Population");
-                long Total_City_Population = rset.getLong("Total_City_Population");
-                long Total_Not_City_Population = rset.getLong("Total_Not_City_Population");
-                String City_Population_Percentage = rset.getString("City_Population_Percentage");
-                String None_City_Population_Percentage = rset.getString("None_City_Population_Percentage");
+
 
                 // Create a RegionPopulationReport object and add it to the list
-                RegionPopulationReport region = new RegionPopulationReport(Region, Total_Region_Population, Total_City_Population, City_Population_Percentage, Total_Not_City_Population, None_City_Population_Percentage);
+                RegionPopulationReport region = new RegionPopulationReport(Region, Total_Region_Population);
                 regionReports.add(region);
             }
             return regionReports;
@@ -101,7 +92,7 @@ public class User_report_9{
         //print report name
         System.out.println("Region Population Report");
         //format and print header
-        System.out.println(String.format("%-20s %-20s %-20s %-20s %-20s %-20s", "Region", "Total_Region_Population", "Total_City_Population", "Total_Not_City_Population", "City_Population_Percentage", "None_City_Population_Percentage"));
+        System.out.println(String.format("%-35s %-20s ", "Region", "Total_Region_Population"));
 
 
         // Iterate through the list of RegionPopulationReport objects and print each one
@@ -111,11 +102,11 @@ public class User_report_9{
                 continue;
             //Prints table values in columbs
             String Table_string =
-                    String.format("%-20s %-20s %-20s %-20s %-20s %-20s",
-                            region.Region, region.Total_Region_Population, region.Total_City_Population, region.Total_Not_City_Population, region.City_Population_Percentage, region.None_City_Population_Percentage);
+                    String.format("%-35s %-20s ",
+                            region.Region, region.Total_Region_Population);
             System.out.println(Table_string);
         }
 
-        }
     }
+}
 
