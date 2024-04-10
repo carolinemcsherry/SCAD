f3f272cc68d17d0d180cf8d001e9c19399db68c8
CMC18 <40626785@live.napier.ac.uk>
2024-04-10 20:44:37 +0100

    gettin country population

--- use_cases_sql/use-case10.sql
+++ use_cases_sql/use-case10.sql
@@ -1,3 +1,4 @@
-select
-population, continent
-from country;--retrieve data on population and continent
+SELECT country.Region,
+                    SUM(country.Population) AS Total_Region_Population
+                    FROM country where country.Region like  + input +  GROUP BY country.Region;
+--retrieve data on population and continent
