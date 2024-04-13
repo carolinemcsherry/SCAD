2f03084471ba4639d903943a7315e782149cb201
CMC18 <40626785@live.napier.ac.uk>
2024-04-13 20:58:41 +0100

    Updating 12 and 13

---
+++ src/main/java/com/napier/sem/User_report_36.java
@@ -0,0 +1,93 @@
+package com.napier.sem;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+//The population of people, people living in cities, and people not living in cities in each continent.
+
+public class User_report_36 {
+
+    // Inner class to represent the population data for each continent
+    public static class ContinentPopulation {
+        private String countryName;
+        private long totalPopulation;
+        private long populationInCities;
+        private long populationNotInCities;
+
+        // Constructor for ContinentPopulation class
+        public ContinentPopulation(String countryName,long totalPopulation,long populationInCities,long populationNotInCities) {
+            this.countryName = countryName;
+            this.totalPopulation = totalPopulation;
+            this.populationInCities = populationInCities;
+            this.populationNotInCities = populationNotInCities;
+        }
+
+        // Method to represent the object as a string
+        public String toString() {
+            return countryName
+                    + totalPopulation
+                    + populationInCities
+                    + populationNotInCities;
+        }
+    }
+
+    // Method to retrieve population data for each continent
+    public static ArrayList<ContinentPopulation> getPopulationByContinent(Connection con) {
+
+        try {
+            Statement stmt = con.createStatement();
+
+            // SQL query to retrieve population data for each continent
+            String strSelect = "SELECT country. as countryName, " +
+                                        "SUM(city.Population) AS PopulationInCities, " +
+                    "(country.Population - SUM(city.Population)) AS PopulationNotInCities " +
+                    "FROM country " +
+                    "LEFT JOIN city ON country.Code = city.CountryCode " +
+                    "GROUP BY country.Name";
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            ArrayList<ContinentPopulation> continentPopulationList = new ArrayList<>();
+
+            // Iterate through the result set and create ContinentPopulation objects
+            while (rset.next()) {
+                String countryName = rset.getString("countryName");
+                long totalPopulation = rset.getLong("TotalPopulation");
+                long populationInCities = rset.getLong("PopulationInCities");
+                long populationNotInCities = rset.getLong("PopulationNotInCities");
+
+                // Create a ContinentPopulation object and add it to the list
+                ContinentPopulation continentPopulation = new ContinentPopulation(countryName, totalPopulation, populationInCities, populationNotInCities);
+                continentPopulationList.add(continentPopulation);
+            }
+            return continentPopulationList;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population data by continent");
+            return null;
+        }
+    }
+
+    // Method to print population data for each continent
+    public static void printPopulationByContinent(ArrayList<ContinentPopulation> continentPopulationList) {
+        if (continentPopulationList == null) {
+            System.out.println("No continentPopulationList");
+            return;
+        }
+
+        System.out.println(String.format("%-35s %-25s %-25s %-25s", "Country Name Name", "Total Population", "Population In Cities", "population Not In Cities"));
+
+        for (ContinentPopulation continentPopulation : continentPopulationList) {
+            if (continentPopulation != null) {
+                String tableString = String.format("%-35s %-25s %-25s %-25s",
+                        continentPopulation.countryName, continentPopulation.totalPopulation, continentPopulation.populationInCities, continentPopulation.populationNotInCities);
+                System.out.println(tableString);
+            }
+        }
+    }
+}
+
