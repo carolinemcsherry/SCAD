ff2341e5f27b28bf2721bbce0de0e284fb4e26e6
CMC18 <40626785@live.napier.ac.uk>
2024-04-13 18:05:59 +0100

    Updating 12 and 13

--- src/main/java/com/napier/sem/User_report_28.java
+++ src/main/java/com/napier/sem/User_report_28.java
@@ -13,26 +13,24 @@ public class User_report_28{
 
     // Inner class to represent population report
     public static class Population {
-        private String cityName;
-        private int cityPopulation;
         private String countryName;
-        private int countryPopulation;
+        private long people_living_in_cities;
+
+        private long people_not_living_in_cities ;
 
         // Constructor for Population class
-        public Population(String cityName, int cityPopulation, String countryName, int countryPopulation) {
-            this.cityName = cityName;
-            this.cityPopulation = cityPopulation;
+        public Population(String countryName, long people_living_in_cities, long people_not_living_in_cities) {
             this.countryName = countryName;
-            this.countryPopulation = countryPopulation;
+            this.people_living_in_cities = people_living_in_cities;
+            this.people_not_living_in_cities = people_not_living_in_cities;
         }
 
         // Method to represent the object as a string
         public String toString() {
-            return cityName
-                    + cityPopulation
-                    + countryName
-                    + countryPopulation;
-        }
+            return countryName
+                    + people_living_in_cities
+                    + people_not_living_in_cities;
+                        }
     }
 
     // Method to retrieve population data
@@ -42,10 +40,7 @@ public class User_report_28{
             Statement stmt = con.createStatement();
 
             // SQL query to retrieve population data
-            String strSelect = "SELECT A.Name AS cityname, A.Population AS city_population, " +
-                    "B.Name AS countryname, B.Population AS country_population " +
-                    "FROM city A " +
-                    "LEFT JOIN country B ON A.CountryCode = B.Code";
+            String strSelect = "SELECT country.Name AS countryName, SUM(city.Population) AS people_living_in_cities, (country.Population - SUM(city.Population)) as people_not_living_in_cities FROM country JOIN city ON country.Code = city.CountryCode where city.CountryCode=country.Code group by country.Name, country.Population ";
 
             ResultSet rset = stmt.executeQuery(strSelect);
 
@@ -53,13 +48,12 @@ public class User_report_28{
 
             // Iterate through the result set and create Population objects
             while (rset.next()) {
-                String cityName = rset.getString("cityname");
-                int cityPopulation = rset.getInt("city_population");
-                String countryName = rset.getString("countryname");
-                int countryPopulation = rset.getInt("country_population");
+                String countryName = rset.getString("countryName");
+                long people_living_in_cities = rset.getLong("people_living_in_cities");
+                long people_not_living_in_cities = rset.getLong("people_not_living_in_cities");
 
                 // Create a Population object and add it to the list
-                Population population = new Population(cityName, cityPopulation, countryName, countryPopulation);
+                Population population = new Population(countryName, people_living_in_cities, people_not_living_in_cities);
                 populationList.add(population);
             }
             return populationList;
@@ -76,11 +70,11 @@ public class User_report_28{
             System.out.println("No populationList");
             return;
         }
-        System.out.println(String.format("%-25s %-25s %-25s %-25s", "cityName", "cityPopulation", "countryName", "countryPopulation"));
+        System.out.println(String.format("%-25s %-25s %-25s", "Country Name", "People Living in Cities", "People Not Living in Cities"));
         for (Population population : populationList) {
             if (population != null) {
-                String tableString = String.format("%-25s %-25s %-25s %-25s",
-                        population.cityName, population.cityPopulation, population.countryName, population.countryPopulation);
+                String tableString = String.format("%-25s %-25s %-25s ",
+                        population.countryName, population.people_living_in_cities, population.people_not_living_in_cities);
                 System.out.println(tableString);
             }
         }
